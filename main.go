bc46a52e376fcb06fad3530fccf5bd41f31a9c9e
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-12-12 14:38:05 +0530

    add contact page

--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to photosplash</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+	fmt.Fprint(w, "Get in touch at <a href=\"mailto:support@photosplash.com\">support@photosplash.com</a>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	http.ListenAndServe(":3000", nil)
 }
