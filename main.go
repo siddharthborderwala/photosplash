01e5ae16e1cb9c0cb68e60052517e25d6c376cfc
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-12-12 15:54:37 +0530

    use gorilla mux for routing

--- main.go
+++ main.go
@@ -3,20 +3,38 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to photosplash</h1>")
 }
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html")
 	fmt.Fprint(w, "Get in touch at <a href=\"mailto:support@photosplash.com\">support@photosplash.com</a>")
 }
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+	params := mux.Vars(r)
+	fmt.Fprintf(w, "Hello %s", params["name"])
+}
+
+func notfound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "The page you are looking for does not exist or has been moved")
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/hello/{name}", hello)
+
+	r.NotFoundHandler = http.HandlerFunc(notfound)
+	http.ListenAndServe(":3000", r)
+	fmt.Println("Server listening at http://localhost:3000")
 }
